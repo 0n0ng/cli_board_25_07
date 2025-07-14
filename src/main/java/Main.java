92158336c4742be9f9cfd2514bbb222a28f7f6cb
0n0ng <hy991006@gmail.com>
2025-07-14 18:31:37 +0900

    리펙토리 2nd/ add controller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-package com.ll;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        new APP(sc).run();
+        new App(sc).run();
 
         sc.close();
     }
