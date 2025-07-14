a09415c137b244cbd8ac7bd9f65cc87075425d35
0n0ng <hy991006@gmail.com>
2025-07-14 15:09:21 +0900

    5단계 해결 완료

--- src/main/java/com/ll/APP.java
+++ src/main/java/com/ll/APP.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class APP {
@@ -8,29 +10,45 @@ public class APP {
     APP(Scanner sc) {
         this.sc = sc;
     }
+
     public void run() {
         System.out.println("==게시판 앱==");
 
-        int lastId =1;
+        int lastId = 1;
+        List<Article> articleList = new ArrayList<>();
+
+//        번호 id = new 번호();
+//        제목 subject = new 제목();
+//        내용 content = new 내용();
 
 
-
-        while(true){
+        while (true) {
             System.out.print("명령)");
             String command = sc.nextLine().trim();
 
-            if(command.equals("종료")){
+            if (command.equals("종료")) {
                 break;
-            }else if (command.equals("등록")){
+            } else if (command.equals("등록")) {
                 System.out.print("제목 :");
                 String text = sc.nextLine().trim();
                 System.out.print("내용 :");
                 String body = sc.nextLine().trim();
 
+                Article article = new Article(lastId, text, body);
+                articleList.add(article);
+
                 System.out.printf("%d번 게시글이 등록되었습니다.\n", lastId);
                 lastId++;
-            }
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 제목 / 내용");
+                System.out.println("-------------------");
 
+                for (int i = 0; i < articleList.size(); i++) {
+                    Article article = articleList.get(i);
+                    System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
+                }
+            }
         }
     }
 }
+
