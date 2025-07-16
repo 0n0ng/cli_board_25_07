ad0edd8e42443350004c82d3153002e223489c91
0n0ng <hy991006@gmail.com>
2025-07-16 15:42:48 +0900

    DB Connection

---
+++ src/main/java/com/ll/db/db.sql
@@ -0,0 +1,18 @@
+-- 기능x 어디서든 사용하기 위한 기록용
+DROP DATABASE IF EXISTS proj1;
+CREATE DATABASE proj1;
+
+USE proj1;
+
+CREATE TABLE article (
+	id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	subject char(100) NOT NULL UNIQUE,
+	content text
+);
+
+INSERT INTO article
+SET id = 3,
+subject = "test3",
+content = "test3"
+
+SELECT * FROM article;
